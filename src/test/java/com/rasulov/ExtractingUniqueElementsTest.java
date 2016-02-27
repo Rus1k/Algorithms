e7d4153ce60f0a37d4e948bc3abf916cc357b3f0
Rus1k <rfrasulov@gmail.com>
2016-02-27 18:05:16 +0200

    add test

--- src/test/java/com/rasulov/ExtractingUniqueElementsTest.java
+++ src/test/java/com/rasulov/ExtractingUniqueElementsTest.java
@@ -1,6 +1,8 @@
 package com.rasulov;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,11 +10,17 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ExtractingUniqueElementsTest {
-    ExtractingUniqueElements extractingUniqueElements = new ExtractingUniqueElements();
-    int [] array;
-    List <Integer> expected;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    public ExtractingUniqueElements extractingUniqueElements = new ExtractingUniqueElements();
+
+    int[] array;
+    List<Integer> expected;
+
     @Test
-    public void testExtractingUniqueElementsTest(){
+    public void testExtractingUniqueElementsTest() {
         array = new int[]{1, 1, 3, 3, 3, 5, 5, 5, 9, 9, 9, 9};
         expected = new ArrayList<Integer>();
         expected.add(1);
@@ -21,5 +29,20 @@ public class ExtractingUniqueElementsTest {
         expected.add(9);
         assertEquals(expected, extractingUniqueElements.extractingUniqueElements(array));
 
+    }
+
+    @Test
+    public void testCheckWithException() {
+        array = null;
+        expectedException.expect(NullPointerException.class);
+        extractingUniqueElements.extractingUniqueElements(array);
+    }
+
+    @Test
+    public void testArrayOfZero(){
+        array = new int []{0, 0, 0, 0, 0, 0};
+        expected = new ArrayList<Integer>();
+        expected.add(0);
+        extractingUniqueElements.extractingUniqueElements(array);
     }
 }
