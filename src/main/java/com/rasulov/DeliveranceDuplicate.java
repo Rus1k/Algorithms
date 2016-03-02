58c892d614dd1497f5b5a89d11c34a7acd6e7413
Rus1k <rfrasulov@gmail.com>
2016-03-02 19:18:15 +0200

    new decision, add test

--- src/main/java/com/rasulov/DeliveranceDuplicate.java
+++ src/main/java/com/rasulov/DeliveranceDuplicate.java
@@ -1,5 +1,7 @@
 package com.rasulov;
 
+//  Write, efficient code for extracting unique elements from a sorted list of array. e.g.
+//  (1, 1, 3, 3, 3, 5, 5, 5, 9, 9, 9, 9) -> (1, 3, 5, 9).
 
 public class DeliveranceDuplicate {
 
---
+++ src/test/java/com/rasulov/DeliveranceDuplicateTest.java
@@ -0,0 +1,71 @@
+package com.rasulov;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+public class DeliveranceDuplicateTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+
+    @Test
+    public void testDeliveranceDuplicate() {
+        Node node4 = new Node(3);
+        Node node3 = new Node(2, node4);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node node = new Node(1, node1);
+        Node actualResult = DeliveranceDuplicate.deliveranceDuplicate(node);
+        Node expectedResult = new Node(1, new Node(2, node4));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testDeliveranceDuplicateWhenAllElementsUnique() {
+        Node node4 = new Node(5);
+        Node node3 = new Node(4, node4);
+        Node node2 = new Node(3, node3);
+        Node node1 = new Node(2, node2);
+        Node node = new Node(1, node1);
+        Node actualResult = DeliveranceDuplicate.deliveranceDuplicate(node);
+        Node expectedResult = new Node(1, new Node(2, new Node(3, new Node(4, node4))));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testDeliveranceDuplicateWhenNotHaveUniqueElements(){
+        Node node5 = new Node(3);
+        Node node4 = new Node(3, node5);
+        Node node3 = new Node(2, node4);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node node = new Node(1, node1);
+        Node actualResult = DeliveranceDuplicate.deliveranceDuplicate(node);
+        Node expectedResult = new Node(1, new Node(2, node5));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testDeliveranceDuplicateWhenDuplicateLastElement(){
+        Node node5 = new Node(5);
+        Node node4 = new Node(5, node5);
+        Node node3 = new Node(4, node4);
+        Node node2 = new Node(3, node3);
+        Node node1 = new Node(2, node2);
+        Node node = new Node(1, node1);
+        Node actualResult = DeliveranceDuplicate.deliveranceDuplicate(node);
+        Node expectedResult = new Node(1, new Node(2, new Node(3, new Node(4, node5))));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testDeliveranceDuplicateWhenNullException(){
+        expectedException.expect(NullPointerException.class);
+        DeliveranceDuplicate.deliveranceDuplicate(null);
+    }
+
+}
